d17f20ace83bd3525d907cef3c5ad369f1d5a5c1
jagger <1357289468@qq.com>
2018-12-26 17:23:42 +0800

    spring-security-helloword

---
+++ src/main/java/com/example/springsecurityhelloword/SpringSecurityHellowordApplication.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityhelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHellowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityHellowordApplication.class, args);
+    }
+
+}
+
